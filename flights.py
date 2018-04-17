7e08637040e28cb0ea0deb37124440c42eddd877
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2018-04-17 20:59:08 +0100

    Add files via upload

---
+++ flights.py
@@ -0,0 +1,63 @@
+from pyswip import *
+
+prolog = Prolog()
+prolog.consult("flights.pl")
+
+def question1():
+    place1 = raw_input("Place1: ")
+    place2 = raw_input("Place2: ")
+    List = Variable()
+    q = Functor("question1",3)
+    result = Query(q(place1, place2, List))
+    while result.nextSolution():
+        for x in List.value:
+            print x
+    result.closeQuery()
+
+def question2():
+    place1 = raw_input("Place1: ")
+    place2 = raw_input("Place2: ")
+    day    = raw_input("Day: ")
+    List = Variable()
+    q = Functor("question2",4)
+    result = Query(q(place1,place2,day,List))
+    while result.nextSolution():
+        for x in List.value:
+            print x
+    result.closeQuery()
+
+def question3():
+    s  = raw_input("Starting location: ")
+    c1 = raw_input("Choose a city to visit: ")
+    c2 = raw_input("Choose a city to visit: ")
+    c3 = raw_input("Choose a city to visit: ")
+    Day  = Variable()
+    List = Variable()
+    q    = Functor("question3", 6)
+    result = Query(q(s,c1,c2,c3,Day,List))
+    while result.nextSolution():
+        for x in List.value:
+            print x
+    result.closeQuery()
+
+questions = {
+    "q1": question1,
+    "q2": question2,
+    "q3": question3
+}
+
+def main():
+    print("Flight planner - Answers specific questions")
+    print("q1: In which days of the week there is a direct flight from Place1 to Place2?")
+    print("q2: What are the available flights from Place1 to Place2 on a certain Day?")
+    print("q3: What order should C1, C2 and C3 be visited, starting and ending at S, with at most 1 flight a day?")
+    print("Enter 'exit' to exit the program")
+    while True:
+        question = raw_input("Which question? ")
+        if question == "exit":
+            break
+        else:
+            questions[question]()
+    print("Exiting parser")
+
+main()
